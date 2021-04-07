e6857a01e61212a79aabd2a7b2821a34c7ba1de7
gparkkii <jyp933@gmail.com>
2021-04-07 18:26:08 +0900

    Context API로 ThemeProvider 커스텀

---
+++ src/context/themeProvider.js
@@ -0,0 +1,41 @@
+import { lightTheme, darkTheme } from '../theme/theme';
+import { createContext, useState, useContext, useCallback } from 'react';
+import { ThemeProvider as StyledProvider } from 'styled-components';
+
+const ThemeContext = createContext({});
+
+const ThemeProvider = ({children}) => {
+  const LocalTheme = window.localStorage.getItem('theme') || 'light';
+  const [ThemeMode, setThemeMode] = useState(LocalTheme);
+  const themeObject = ThemeMode === 'light' ? lightTheme : darkTheme;
+
+  return(
+    <ThemeContext.Provider value={{ ThemeMode, setThemeMode }}>
+      <StyledProvider theme={themeObject}>
+        { children }
+      </StyledProvider>      
+    </ThemeContext.Provider>
+  )
+}
+
+function useTheme() {
+  const context = useContext(ThemeContext);
+  const { ThemeMode, setThemeMode } = context;
+
+  const toggleTheme = useCallback(() => {
+    if (ThemeMode === "light") {
+      setThemeMode("dark");
+      window.localStorage.setItem('theme', 'dark');
+    }
+    else {
+      setThemeMode("light")
+      window.localStorage.setItem('theme', 'light');
+    };
+  }, [ThemeMode]);
+  
+  return [ ThemeMode, toggleTheme];
+}
+
+export { ThemeProvider, useTheme };
+
+export default ThemeProvider;
