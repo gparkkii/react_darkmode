0abc4581536e11fb53a60582e6ed34d90ef44694
gparkkii <jyp933@gmail.com>
2021-04-09 14:33:25 +0900

    fix :arrow_forward: 필요없는 코드 지우기

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,14 +2,12 @@ export const lightTheme = {
   bgColor: '#F8F7F4',
   textColor: '#31302E',
   borderColor: '1px solid #eaeaea',
-  isDark: false,
 };
 
 export const darkTheme = {
   bgColor: '#1E1E22',
   textColor: '#ccc',
   borderColor: '1px solid #2c2d33',
-  isDark: true,
 };
 
 export const theme = {
